d09570a80d5599d3116de73ddd6b182bab96b8a4
Jeremy Lilje <jeremylilje@Jeremys-MacBook-Pro.local>
2016-06-15 10:26:03 -0400

    initial commit

---
+++ LawnBoy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LawnBoy
+//
+//  Created by Jeremy Lilje on 6/15/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LawnBoy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LawnBoy
+//
+//  Created by Jeremy Lilje on 6/15/16.
+//  Copyright © 2016 Detroit Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
